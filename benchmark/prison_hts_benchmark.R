93c4a52ce2cc4f6e41846cbb18b1f9c4e77710f2
mach1ne <luis_roque@live.com>
2021-01-05 17:19:00 +0000

    benchmark using Hyndman model

---
+++ benchmark/prison_hts_benchmark.R
@@ -0,0 +1,106 @@
+prison <- readr::read_csv("/home/mach1ne/hierarchical_ts_forecaster/data/prison_to_r.csv") %>%
+  mutate(Quarter = yearquarter(Date)) %>%
+  select(-Date)  %>%
+  as_tsibble(key = c(Gender, Legal, State), index = Quarter) %>%
+  relocate(Quarter)
+
+
+prison_gts <- prison %>%
+  aggregate_key(Gender * Legal * State, Count = sum(Count)/1e3)
+
+fit <- prison_gts %>%
+  filter(year(Quarter) <= 2014) %>%
+  model(base = ETS(Count)) %>%
+  reconcile(
+    bottom_up = bottom_up(base),
+    MinT = min_trace(base, method = "mint_shrink")
+  )
+fc <- fit %>% forecast(h = 8)
+
+# State
+results_state = fc %>%
+  filter(
+    is_aggregated(Legal), is_aggregated(Gender)
+  ) %>%
+  accuracy(data = prison_gts, measures = list(
+    mase = MASE,
+    rmse = RMSE
+  )) %>%
+  group_by(.model) %>%
+  summarise(mase = mean(mase), rmse = mean(rmse)*1e3)
+results_state = results_state %>% add_column(group = 'state', .before=".model")
+results = results_state
+
+# Gender
+results_gender = fc %>%
+  filter(
+    is_aggregated(Legal), is_aggregated(State)
+  ) %>%
+  accuracy(data = prison_gts, measures = list(
+    mase = MASE,
+    rmse = RMSE
+  )) %>%
+  group_by(.model) %>%
+  summarise(mase = mean(mase), rmse = mean(rmse)*1e3)
+results_gender = results_gender %>% add_column(group = 'gender', .before=".model")
+results = full_join(results, results_gender)
+
+# Legal
+results_legal = fc %>%
+  filter(
+    is_aggregated(Gender), is_aggregated(State)
+  ) %>%
+  accuracy(data = prison_gts, measures = list(
+    mase = MASE,
+    rmse = RMSE
+  )) %>%
+  group_by(.model) %>%
+  summarise(mase = mean(mase), rmse = mean(rmse)*1e3)
+results_legal = results_legal %>% add_column(group = 'legal', .before=".model")
+results = full_join(results, results_legal)
+
+# Total
+results_tot = fc %>%
+  filter(
+    is_aggregated(Legal), is_aggregated(Gender), is_aggregated(State)
+  ) %>%
+  accuracy(data = prison_gts, measures = list(
+    mase = MASE,
+    rmse = RMSE
+  )) %>%
+  group_by(.model) %>%
+  summarise(mase = mean(mase), rmse = mean(rmse)*1e3)
+results_tot = results_tot %>% add_column(group = 'total', .before=".model")
+results = full_join(results, results_tot)
+
+
+# All
+results_all = fc %>%
+  accuracy(data = prison_gts, measures = list(
+    mase = MASE,
+    rmse = RMSE
+  )) %>%
+  group_by(.model) %>%
+  summarise(mase = mean(mase), rmse = mean(rmse)*1e3)
+results_all = results_all %>% add_column(group = 'all', .before=".model")
+results = full_join(results, results_all)
+
+
+# Bottom
+results_bot = fc %>% 
+  filter(
+    !is_aggregated(Legal), !is_aggregated(Gender), !is_aggregated(State)
+  ) %>%
+  accuracy(data = prison_gts, measures = list(
+    mase = MASE,
+    rmse = RMSE
+  )) %>%
+  group_by(.model) %>%
+  summarise(mase = mean(mase), rmse = mean(rmse)*1e3)
+results_bot = results_bot %>% add_column(group = 'bot', .before=".model")
+results = full_join(results, results_bot)
+
+
+results = results %>% filter(!(.model=='base'))
+results
+
---
+++ benchmark/tourism_hts_benchmark.R
@@ -0,0 +1,121 @@
+tourism <- readr::read_csv("/home/mach1ne/hierarchical_ts_forecaster/data/tourism_to_r.csv") %>%
+  mutate(YearMonth = yearmonth(Date)) %>%
+  select(-Date)  %>%
+  as_tsibble(key = c(State, Zone, Region, Purpose), index = YearMonth) %>%
+  relocate(YearMonth)
+
+
+tourism_gts <- tourism %>%
+  aggregate_key((State / Zone / Region) * Purpose, Count = sum(Count))
+
+fit <- tourism_gts %>%
+  filter(YearMonth <= yearmonth(as.Date("2015-02-01"))) %>%
+  model(base = ETS(Count)) %>%
+  reconcile(
+    bottom_up = bottom_up(base),
+    MinT = min_trace(base, method = "mint_shrink")
+  )
+  
+fc <- fit %>% forecast(h = 8)
+
+# State
+results_state = fc %>%
+  filter(
+    is_aggregated(Zone), is_aggregated(Region), is_aggregated(Purpose)
+  ) %>%
+  accuracy(data = tourism_gts, measures = list(
+    mase = MASE,
+    rmse = RMSE
+  )) %>%
+  group_by(.model) %>%
+  summarise(mase = mean(mase), rmse = mean(rmse))
+results_state = results_state %>% add_column(group = 'state', .before=".model")
+results = results_state
+
+# Zone
+results_zone = fc %>%
+  filter(
+    is_aggregated(State), is_aggregated(Region), is_aggregated(Purpose)
+  ) %>%
+  accuracy(data = tourism_gts, measures = list(
+    mase = MASE,
+    rmse = RMSE
+  )) %>%
+  group_by(.model) %>%
+  summarise(mase = mean(mase), rmse = mean(rmse))
+results_zone = results_zone %>% add_column(group = 'zone', .before=".model")
+results = full_join(results, results_zone)
+
+# region
+results_region = fc %>%
+  filter(
+    is_aggregated(State), is_aggregated(Zone), is_aggregated(Purpose)
+  ) %>%
+  accuracy(data = tourism_gts, measures = list(
+    mase = MASE,
+    rmse = RMSE
+  )) %>%
+  group_by(.model) %>%
+  summarise(mase = mean(mase), rmse = mean(rmse))
+results_region = results_region %>% add_column(group = 'region', .before=".model")
+results = full_join(results, results_region)
+
+# purpose
+results_purpose = fc %>%
+  filter(
+    is_aggregated(State), is_aggregated(Zone), is_aggregated(Region)
+  ) %>%
+  accuracy(data = tourism_gts, measures = list(
+    mase = MASE,
+    rmse = RMSE
+  )) %>%
+  group_by(.model) %>%
+  summarise(mase = mean(mase), rmse = mean(rmse))
+results_purpose = results_purpose %>% add_column(group = 'purpose', .before=".model")
+results = full_join(results, results_purpose)
+
+# Total
+results_tot = fc %>%
+  filter(
+    is_aggregated(State), is_aggregated(Zone), is_aggregated(Region), is_aggregated(Purpose)
+  ) %>%
+  accuracy(data = tourism_gts, measures = list(
+    mase = MASE,
+    rmse = RMSE
+  )) %>%
+  group_by(.model) %>%
+  summarise(mase = mean(mase), rmse = mean(rmse))
+results_tot = results_tot %>% add_column(group = 'total', .before=".model")
+results = full_join(results, results_tot)
+
+
+# All
+results_all = fc %>%
+  accuracy(data = tourism_gts, measures = list(
+    mase = MASE,
+    rmse = RMSE
+  )) %>%
+  group_by(.model) %>%
+  summarise(mase = mean(mase), rmse = mean(rmse))
+results_all = results_all %>% add_column(group = 'all', .before=".model")
+results = full_join(results, results_all)
+
+
+# Bottom
+results_bot = fc %>% 
+  filter(
+    !is_aggregated(State), !is_aggregated(Zone), !is_aggregated(Region), !is_aggregated(Purpose)
+  ) %>%
+  accuracy(data = tourism_gts, measures = list(
+    mase = MASE,
+    rmse = RMSE
+  )) %>%
+  group_by(.model) %>%
+  summarise(mase = mean(mase), rmse = mean(rmse))
+results_bot = results_bot %>% add_column(group = 'bot', .before=".model")
+results = full_join(results, results_bot)
+
+
+results = results %>% filter(!(.model=='base'))
+results
+
